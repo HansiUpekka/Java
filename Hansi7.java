62d9308540954a27c5ac591cdbddcea2db8533d0
HansiUpekka <hansiupekkasithumini2002@gmail.com>
2025-05-07 22:41:25 +0530

    Add files via upload

---
+++ Hansi7.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Hansi7
+{
+    public static void main(String[] args)
+    {
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //int score = 95;
+        System.out.print("Enter your score: ");
+        double score = scanner.nextDouble();
+
+        if (score >= 60.00)
+        {
+            System.out.println("Pass");
+        }
+        else
+        {
+            System.out.println("Work Hard");
+        }
+
+        String passOrFail = (score >= 60.00) ? "Pass" : "Work Hard";
+        System.out.println(passOrFail);
+
+        System.out.print("Enter an integer number: ");
+        int number = scanner.nextInt();
+
+        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(evenOrOdd);
+
+        System.out.print("Enter time: ");
+        double time = scanner.nextDouble();
+
+        String dayOrNight = (time < 12.00) ? "It is A.M." : "It is P.M.";
+        System.out.println(dayOrNight);
+
+        System.out.print("Enter your salary: ");
+        double salary = scanner.nextDouble();
+
+        String payTaxOrNot = (salary >= 50000) ? "Pay Tax" : "No need to pay tax";
+        System.out.println(payTaxOrNot);
+
+        scanner.close();
+
+    }
+}
