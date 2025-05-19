210d6592fb7c39d15332da0c3b50bcd316ac33ad
Hansi Upekka Sithumini <hansiupekkasithumini2002@gmail.com>
2025-05-20 00:14:18 +0530

    Add files via upload

--- Hansi17.java
+++ Hansi17.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hansi17
 {
     public static void main(String[] args)
@@ -7,14 +9,61 @@ public class Hansi17
         String name = "Hansi";
         int age = 22;
 
-        happyBirthday(name);
+        happyBirthday(name,age);
+
+        System.out.println();
+
+        double result = square(2);
+        System.out.println(result);  // System.out.println(square(2));
+
+        String fullName = getFullName("Hansi","Upekka");
+        String fullName1 = getFullName("Hansi","Upekka Sithumini");
+        System.out.println(fullName1);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int myage = scanner.nextInt();
+
+        if (checkAge(myage))
+        {
+            System.out.println("You may be signed in");
+        }
+        else
+        {
+            System.out.println("You must be 18+ years old");
+        }
+
     }
 
-    static void  happyBirthday(String name)
+    static void  happyBirthday(String birthday_girl, int age)
     {
         System.out.println("Happy Birthday to you...");
         System.out.println("Happy Birthday to you...");
-        System.out.printf("Happy Birthday dear %s ...\n",name);
+        System.out.printf("Happy Birthday dear %s ...\n",birthday_girl);
         System.out.println("Happy Birthday to you...");
+        System.out.printf("Hey now you are %d years old.",age);
+    }
+
+    static double square (double number)
+    {
+        return number * number;
+    }
+
+    static String getFullName(String firstName, String lastName)
+    {
+        return firstName + " " + lastName;
+    }
+
+    static boolean checkAge (int yourage)
+    {
+        if(yourage >= 18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
