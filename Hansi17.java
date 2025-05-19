249bb4a6e5298059700c1846a391bea62be348cc
Hansi Upekka Sithumini <hansiupekkasithumini2002@gmail.com>
2025-05-19 05:33:08 +0530

    Add files via upload

---
+++ Hansi17.java
@@ -0,0 +1,20 @@
+public class Hansi17
+{
+    public static void main(String[] args)
+    {
+        //using methods.
+
+        String name = "Hansi";
+        int age = 22;
+
+        happyBirthday(name);
+    }
+
+    static void  happyBirthday(String name)
+    {
+        System.out.println("Happy Birthday to you...");
+        System.out.println("Happy Birthday to you...");
+        System.out.printf("Happy Birthday dear %s ...\n",name);
+        System.out.println("Happy Birthday to you...");
+    }
+}
